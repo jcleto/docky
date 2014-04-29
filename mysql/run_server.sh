075c9d49223b615d9d2a23fd998f2c7980da6e7b
Joao <jcleto@irdeto.com>
2014-04-29 14:09:32 +0200

    mysql docker

---
+++ mysql/run_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker run -d -p 3306:3306 -v /data/mysql:/var/lib/mysql centos:mysql
+docker run -d -p 3306:3306 centos:mysql
---
+++ mysql/startup.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    mysql_install_db
+
+    /usr/bin/mysqld_safe &
+    sleep 10s
+
+    echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+    echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+    mysql < /opt/database/create_database_users.sql
+
+    killall mysqld
+    sleep 10s
+fi
+
+/usr/bin/mysqld_safe
+
